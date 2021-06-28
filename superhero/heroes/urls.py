91b982c7d08683f73e39467e147832483fae6971
carter-willey <cjwilley23@gmail.com>
2021-06-28 13:59:45 -0500

    added new urls and changed initial load url

--- superhero/heroes/urls.py
+++ superhero/heroes/urls.py
@@ -1,1 +1,6 @@
 from . import views
+from django.urls import path
+app_name = 'heroes'
+urlpatterns = [
+    path('', views.index, name='index')
+]
--- superhero/superhero/urls.py
+++ superhero/superhero/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('heroes/', include('heroes.urls'))
+    path('heroes/', include('heroes.urls')),
+    path('', RedirectView.as_view(url='/heroes/'))
 ]
